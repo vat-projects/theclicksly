b13aef69803b55468a58d26da22a2f6d12dd5a3f
SergWebAcceptance <serg.web@acceptance.ltd>
2025-05-14 09:11:50 +0300

    added phone

--- src/shared/lib/constants/constants.ts
+++ src/shared/lib/constants/constants.ts
@@ -1,5 +1,5 @@
 export const WEBSITE_EMAIL = "info@theclicksly.com";
-export const WEBSITE_PHONE = "+000 000 00 00";
+export const WEBSITE_PHONE = "+447426463899";
 export const WEBSITE_REGISTRATION_ADDRESS = "Intershore Chambers, Road Town, Tortola, British Virgin Islands";
 export const WEBSITE_OFFICE_ADDRESS = "Floor G, Arthaus, 205 Richmond Road, London E8 3FF, United Kingdom";
 
