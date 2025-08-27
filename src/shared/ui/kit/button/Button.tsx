91d84d85166b705a983ce965239b6be48041a622
Daniil <dan.developer@born2be.media>
2025-08-27 17:26:14 +0300

    [feature/captcha]: ReCaptcha (#2)

--- src/shared/ui/kit/button/Button.tsx
+++ src/shared/ui/kit/button/Button.tsx
@@ -1,24 +1,26 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-import { Arrow } from "../../icons/arrow/arrow";
-import styles from "./Button.module.scss";
+import { Arrow } from '../../icons/arrow/arrow';
+import styles from './Button.module.scss';
 
 export default function Button({
   url,
   type,
   children,
-  color = "black",
-  buttonType = "button",
+  color = 'black',
+  buttonType = 'button',
+  disabled = false,
 }: {
   url?: string;
   type?: string;
   children: React.ReactNode;
-  color?: "black" | "green" | "green-white";
-  buttonType?: "button" | "submit" | "reset";
+  color?: 'black' | 'green' | 'green-white';
+  buttonType?: 'button' | 'submit' | 'reset';
+  disabled?: boolean;
 }) {
-  if (type === "link") {
+  if (type === 'link') {
     return (
-      <Link href={url || ""} className={styles.button + " " + styles[color]}>
+      <Link href={url || ''} className={styles.button + ' ' + styles[color]}>
         <span>{children}</span>
         <div className={styles.arrow}>
           <Arrow />
@@ -27,7 +29,7 @@ export default function Button({
     );
   }
   return (
-    <button className={styles.button + " " + styles[color]} type={buttonType}>
+    <button className={styles.button + ' ' + styles[color]} type={buttonType} disabled={disabled}>
       <span>{children}</span>
       <div className={styles.arrow}>
         <Arrow />
